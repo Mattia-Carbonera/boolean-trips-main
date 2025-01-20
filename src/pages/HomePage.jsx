08e9f57e59375c5d32fb2582ddb3eb4ae2b8d367
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-20 14:46:11 +0100

    fix HomePage Container

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,34 +15,36 @@ export default function HomePage() {
   return (
     <>
       <Header page="home" isDisplayedHandler={handleMenuDisplayed} />
-      <main className="container px-4 mt-4 mb-10 h-100">
-        <h1 className="mb-1 text-center fw-bold">I TUOI VIAGGI ATTIVI</h1>
-        <div className="card-trip-container">
-          {trips.map((trip) => {
-            return <Card key={trip.id} trip={trip}></Card>;
-          })}
-        </div>
-        <div className={`user-menu ${menuDisplayed}`}>
-          <div className="user-welcome">
-            <span>
-              <strong>benvenuto Marco!</strong>
-            </span>
+      <main className="px-4 mt-4 mb-10 h-100">
+        <div className="container">
+          <h1 className="mb-1 text-center fw-bold">I TUOI VIAGGI ATTIVI</h1>
+          <div className="card-trip-container">
+            {trips.map((trip) => {
+              return <Card key={trip.id} trip={trip}></Card>;
+            })}
           </div>
-          <ul>
-            <li>Il tuo profilo</li>
-            <hr />
-            <Link className="link-user-menu" to={"/new-trip"}>
-              <li>Aggiungi viaggio</li>
-            </Link>
-            <hr />
-            <Link className="link-user-menu" to={"/new-contact"}>
-              <li>Aggiungi contatto</li>
-            </Link>
-            <hr />
-            <Link className="link-user-menu" to={"/contacts"}>
-              <li>Contatti</li>
-            </Link>
-          </ul>
+          <div className={`user-menu ${menuDisplayed}`}>
+            <div className="user-welcome">
+              <span>
+                <strong>benvenuto Marco!</strong>
+              </span>
+            </div>
+            <ul>
+              <li>Il tuo profilo</li>
+              <hr />
+              <Link className="link-user-menu" to={"/new-trip"}>
+                <li>Aggiungi viaggio</li>
+              </Link>
+              <hr />
+              <Link className="link-user-menu" to={"/new-contact"}>
+                <li>Aggiungi contatto</li>
+              </Link>
+              <hr />
+              <Link className="link-user-menu" to={"/contacts"}>
+                <li>Contatti</li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </main>
     </>
